4668e0ac6988a8e401078cd44d79923c0d982ef6
Jay <19259608+jayhill365@users.noreply.github.com>
2023-02-28 12:34:57 -0800

    Update monero-miner.sh

--- monero-miner.sh
+++ monero-miner.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 # Gather dependencies
-miner_url="https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-linux-x64.tar.gz"
+miner_url="https://github.com/xmrig/xmrig/releases/download/v6.19.0/xmrig-6.19.0-macos-x64.tar.gz"
 config_url="https://raw.githubusercontent.com/xmrig/xmrig/master/src/config.json"
 
 # Download miner
 wget $miner_url
-tar -xvzf xmrig-6.19.0-linux-x64.tar.gz
-rm xmrig-6.19.0-linux-x64.tar.gz
+tar -xvzf xmrig-6.19.0-macos-x64.tar.gz
+rm xmrig-6.19.0-macos-x64.tar.gz
 
 # Download config file
 wget $config_url -O xmrig-6.19.0/config.json
 
 # Set sending address
 address="WALLETADD"
-sed -i "s/YOUR_WALLET_ADDRESS/$address/g" /xmrig-6.19.0/config.json
+sed -i .bak "s/YOUR_WALLET_ADDRESS/$address/g" /xmrig-6.19.0/config.json
 
 
 # Start miner
 cd xmrig-6.19.0/
 chmod +x xmrig
-nohup ./xmrig --config=config.json & disown
+
+# disconnects the process from the terminal but keeps it running in the background 
+# nohup ./xmrig --config=config.json & disown
+
+#runs in terminal
+./xmrig --config=config.json
 
 # Add a cron job to remove the nohup.out file every hour
 (crontab -l 2>/dev/null; echo "0 * * * * rm nohup.out") | crontab -
